5dbf59733838e45ef3e42001face12cceff22f26
VADDAVALLI SAI SITA RAM <saisitaramvaddavalli@gmail.com>
2021-04-28 18:42:16 +0530

    created footer component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Header from './components/Header';
 import ListUsers from './components/ListUsers';
+import Footer from './components/Footer';
 import './App.css';
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <div className="App">
         <Header/>
         <ListUsers/>
+        <Footer/>
     </div>
   );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './Footer.css';
+class Footer extends Component {
+    render() {
+        return (
+            <div className="faq-footer">
+                <div className="faq-footer-text">Made with <span className="faq-footer-heart"> &hearts;</span> by Sai Sita Ram</div>
+            </div>
+        );
+    }
+}
+
+export default Footer;
